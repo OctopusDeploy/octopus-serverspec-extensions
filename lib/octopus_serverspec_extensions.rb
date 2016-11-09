d10d5e36127f2dcb0f61fd645bfddf6d967f1433
Matt Richardson <matt@redgumtech.com.au>
2016-11-09 21:51:19 +1100

    Bump version number

--- lib/octopus_serverspec_extensions.rb
+++ lib/octopus_serverspec_extensions.rb
@@ -1,3 +1,4 @@
 require 'octopus_serverspec_extensions/chocolatey_package.rb'
 require 'octopus_serverspec_extensions/npm_package.rb'
+require 'octopus_serverspec_extensions/java_property_file.rb'
 require 'octopus_serverspec_extensions/version.rb'
