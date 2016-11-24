8c8423b09fbd766916fe120bec096d6f4f071e1a
Matt Richardson <matt@redgumtech.com.au>
2016-11-24 13:30:12 +1100

    Make `octopus_deploy_tentacle .online?` more deterministic

--- lib/octopus_serverspec_extensions/type/octopus_deploy_tentacle.rb
+++ lib/octopus_serverspec_extensions/type/octopus_deploy_tentacle.rb
@@ -5,13 +5,12 @@ require 'json'
 
 module Serverspec::Type
   class OctopusDeployTentacle < Base
-    @body = nil
     @machine = nil
     @serverUrl = nil
     @apiKey = nil
 
     def initialize(serverUrl, apiKey, instance)
-      @name = "Octopus Deploy Tentacle"
+      @name = "Octopus Deploy Tentacle #{instance}"
       @runner = Specinfra::Runner
       @serverUrl = serverUrl
       @apiKey = apiKey
@@ -25,19 +24,9 @@ module Serverspec::Type
         return
       end
 
-      url = "#{serverUrl}/api/machines/all?api-key=#{apiKey}"
-      begin
-        resp = Net::HTTP.get_response(URI.parse(url))
-        @body = JSON.parse(resp.body)
-        if (instance.nil?)
-          thumbprint = `"c:\\program files\\Octopus Deploy\\Tentacle\\Tentacle.exe" --console --show-thumbprint --nologo`.strip.gsub('The thumbprint of this Tentacle is: ', '')
-        else
-          @name = instance
-          thumbprint = `"c:\\program files\\Octopus Deploy\\Tentacle\\Tentacle.exe" --console --show-thumbprint --nologo --instance #{instance}`.strip.gsub('The thumbprint of this Tentacle is: ', '')
-        end
-        @machine = @body.select {|e| e["Thumbprint"] == thumbprint}.first
-      rescue => e
-        puts "Unable to connect to #{url}"
+      if (exists?)
+        thumbprint = `"c:\\program files\\Octopus Deploy\\Tentacle\\Tentacle.exe" --console --show-thumbprint --nologo --instance #{instance}`.strip.gsub('The thumbprint of this Tentacle is: ', '')
+        @machine = get_machine_via_api(serverUrl, apiKey, thumbprint)
       end
     end
 
@@ -47,7 +36,8 @@ module Serverspec::Type
 
     def online?
       return nil if @machine.nil?
-      puts "Expected status 'Online' or 'CalamariNeedsUpgrade', but got '#{@machine["Status"]}'" if (@machine["Status"] != "Online" && @machine["Status"] != "CalamariNeedsUpgrade")
+      @machine = poll_until_machine_has_completed_healthcheck(@serverUrl, @apiKey, @machine["Thumbprint"])
+      puts "Expected status 'Online' or 'CalamariNeedsUpgrade' for Tentacle #{@name}, but got '#{@machine["Status"]}'" if (@machine["Status"] != "Online" && @machine["Status"] != "CalamariNeedsUpgrade")
       @machine["Status"] == "Online" || @machine["Status"] == "CalamariNeedsUpgrade"
     end
 
@@ -68,19 +58,64 @@ module Serverspec::Type
 
     def listening_tentacle?
       return false if @machine.nil?
-      puts "Expected CommunicationStyle 'TentaclePassive', but got '#{@machine["EndPoint"]["CommunicationStyle"]}'" if (@machine["EndPoint"]["CommunicationStyle"] != "TentaclePassive")
+      puts "Expected CommunicationStyle 'TentaclePassive' for Tentacle #{@name}, but got '#{@machine["EndPoint"]["CommunicationStyle"]}'" if (@machine["EndPoint"]["CommunicationStyle"] != "TentaclePassive")
       @machine["EndPoint"]["CommunicationStyle"] == "TentaclePassive"
     end
 
     def polling_tentacle?
       return false if @machine.nil?
-      puts "Expected CommunicationStyle 'TentacleActive', but got '#{@machine["EndPoint"]["CommunicationStyle"]}'" if (@machine["EndPoint"]["CommunicationStyle"] != "TentacleActive")
+      puts "Expected CommunicationStyle 'TentacleActive' for Tentacle #{@name}, but got '#{@machine["EndPoint"]["CommunicationStyle"]}'" if (@machine["EndPoint"]["CommunicationStyle"] != "TentacleActive")
       @machine["EndPoint"]["CommunicationStyle"] == "TentacleActive"
+    end
+
+    def exists?
+      ::File.exists?("c:\\program files\\Octopus Deploy\\Tentacle\\Tentacle.exe")
     end
   end
 
   def octopus_deploy_tentacle(serverUrl, apiKey, instance)
     OctopusDeployTentacle.new(serverUrl, apiKey, instance)
+  end
+
+  private
+
+  def poll_until_machine_has_completed_healthcheck(serverUrl, apiKey, thumbprint)
+    machine = nil
+    url = "#{serverUrl}/api/machines/all?api-key=#{apiKey}"
+
+    now = Time.now
+    counter = 1
+    loop do
+      machine = get_machine_via_api(serverUrl, apiKey, thumbprint)
+
+      break if machine.nil?
+      break if counter > 10
+      break if !machine_healthcheck_outstanding(machine)
+      puts "Machine health check for #{machine["Name"]} has not yet completed. Waiting 5 seconds to try again."
+      counter += 1
+      sleep 5
+    end
+
+    machine
+  end
+
+  def machine_healthcheck_outstanding(machine)
+    machine["StatusSummary"] == "This machine was recently added. Please perform a health check."
+  end
+
+  def get_machine_via_api(serverUrl, apiKey, thumbprint)
+    machine = nil
+    url = "#{serverUrl}/api/machines/all?api-key=#{apiKey}"
+
+    begin
+      resp = Net::HTTP.get_response(URI.parse(url))
+      body = JSON.parse(resp.body)
+      machine = body.select {|e| e["Thumbprint"] == thumbprint}.first unless body.nil?
+    rescue => e
+      puts "Unable to connect to #{url}: #{e}"
+    end
+
+    machine
   end
 end
 
