529daf35230e3b947a8745b816cb5fadcb66b362
Matt Richardson <matt.richardson@octopus.com>
2017-11-16 08:32:10 +1100

    Add type for checking octopus deploy environment exists

---
+++ lib/octopus_serverspec_extensions/type/octopus_deploy_environment.rb
@@ -0,0 +1,57 @@
+require 'serverspec'
+require 'serverspec/type/base'
+require 'net/http'
+require 'json'
+
+module Serverspec::Type
+  class OctopusDeployEnvironment < Base
+    @environment = nil
+    @serverUrl = nil
+    @apiKey = nil
+
+    def initialize(serverUrl, apiKey, environment_name)
+      @name = "Octopus Deploy Environment #{environment_name}"
+      @runner = Specinfra::Runner
+      @serverUrl = serverUrl
+      @apiKey = apiKey
+
+      if (serverUrl.nil?)
+        puts "'serverUrl' was not provided. Unable to connect to Octopus server to validate configuration."
+        return
+      end
+      if (apiKey.nil?)
+        puts "'apiKey' was not provided. Unable to connect to Octopus server to validate configuration."
+        return
+      end
+
+      @environment = get_environment_via_api(serverUrl, apiKey, thumbprint)
+    end
+
+    def exists?
+      !@environment.nil?
+    end
+  end
+
+  def octopus_deploy_tentacle(serverUrl, apiKey, environment_name)
+    OctopusDeployEnvironment.new(serverUrl, apiKey, environment_name)
+  end
+
+  private
+
+  def get_environment_via_api(serverUrl, apiKey, environment_name)
+    environment = nil
+    url = "#{serverUrl}/api/environments?name=#{environment_name}&api-key=#{apiKey}"
+
+    begin
+      resp = Net::HTTP.get_response(URI.parse(url))
+      body = JSON.parse(resp.body)
+      environment = body['Items'].first unless body.nil?
+    rescue => e
+      puts "Unable to connect to #{url}: #{e}"
+    end
+
+    environment
+  end
+end
+
+include Serverspec::Type
