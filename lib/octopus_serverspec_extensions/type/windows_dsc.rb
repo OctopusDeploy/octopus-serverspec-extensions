b2cf43d135a6954b7d0b0a606931cb40141e89a6
Matt Richardson <matt.richardson@octopus.com>
2017-11-17 05:58:47 +1100

    Add type to test DSC configuration status

---
+++ lib/octopus_serverspec_extensions/type/windows_dsc.rb
@@ -0,0 +1,32 @@
+require 'serverspec'
+require 'serverspec/type/base'
+
+module Serverspec::Type
+  class WindowsDSC < Base
+
+    def initialize
+      @runner = Specinfra::Runner
+    end
+
+    def able_to_get_dsc_configuration?(port)
+      command_result = @runner.run_command('$ProgressPreference = "SilentlyContinue"; try { Get-DSCConfiguration -Force -ErrorAction Stop; write-output "Get-DSCConfiguration succeeded"; $true } catch { write-output "Get-DSCConfiguration failed"; write-output $_; $false }')
+      command_result.stdout.gsub(/\n/, '').match /Get-DSCConfiguration succeeded/
+    end
+
+    def able_to_test_dsc_configuration?(port)
+      command_result = @runner.run_command('$ProgressPreference = "SilentlyContinue"; try { if (-not (Test-DSCConfiguration -Force -ErrorAction Stop)) { write-output "Test-DSCConfiguration returned false"; exit 1 } write-output "Test-DSCConfiguration succeeded"; exit 0 } catch { write-output "Test-DSCConfiguration failed"; write-output $_; exit 2 }')
+      command_result.stdout.gsub(/\n/, '').match /Test-DSCConfiguration succeeded/
+    end
+
+    def to_s
+      "Windows DSC"
+    end
+
+  end
+
+  def windows_dsc
+    WindowsDSC.new
+  end
+end
+
+include Serverspec::Type
--- lib/octopus_serverspec_extensions/version.rb
+++ lib/octopus_serverspec_extensions/version.rb
@@ -1,3 +1,3 @@
 module OctopusServerSpecExtensions
-  VERSION = "0.12.2"
+  VERSION = "0.13.0"
 end
