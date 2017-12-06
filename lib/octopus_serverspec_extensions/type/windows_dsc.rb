dfec8fb0a9e601a65b9a8db90acb5ffb3f9c815c
Matt Richardson <matt.richardson@octopus.com>
2017-12-06 12:26:22 +1100

    Wait until LCM state is idle before trying to check DSC state

--- lib/octopus_serverspec_extensions/type/windows_dsc.rb
+++ lib/octopus_serverspec_extensions/type/windows_dsc.rb
@@ -9,12 +9,12 @@ module Serverspec::Type
     end
 
     def able_to_get_dsc_configuration?
-      command_result = @runner.run_command('$ProgressPreference = "SilentlyContinue"; try { Get-DSCConfiguration -ErrorAction Stop; write-output "Get-DSCConfiguration succeeded"; $true } catch { write-output "Get-DSCConfiguration failed"; write-output $_; $false }')
+      command_result = @runner.run_command('$ProgressPreference = "SilentlyContinue"; $state = ""; do { $state = (Get-DscLocalConfigurationManager).LCMState; write-host "LCM state is $state"; Start-Sleep -Seconds 2; } while ($state -ne "Idle"); try { Get-DSCConfiguration -ErrorAction Stop; write-output "Get-DSCConfiguration succeeded"; $true } catch { write-output "Get-DSCConfiguration failed"; write-output $_; $false }')
       command_result.stdout.gsub(/\n/, '').match /Get-DSCConfiguration succeeded/
     end
 
     def applied_dsc_configuration_successfully?
-      command_result = @runner.run_command('$ProgressPreference = "SilentlyContinue"; try { if (-not (Test-DSCConfiguration -ErrorAction Stop)) { write-output "Test-DSCConfiguration returned false"; exit 1 } write-output "Test-DSCConfiguration succeeded"; exit 0 } catch { write-output "Test-DSCConfiguration failed"; write-output $_; exit 2 }')
+      command_result = @runner.run_command('$ProgressPreference = "SilentlyContinue"; $state = ""; do { $state = (Get-DscLocalConfigurationManager).LCMState; write-host "LCM state is $state"; Start-Sleep -Seconds 2; } while ($state -ne "Idle"); try { if (-not (Test-DSCConfiguration -ErrorAction Stop)) { write-output "Test-DSCConfiguration returned false"; exit 1 } write-output "Test-DSCConfiguration succeeded"; exit 0 } catch { write-output "Test-DSCConfiguration failed"; write-output $_; exit 2 }')
       command_result.stdout.gsub(/\n/, '').match /Test-DSCConfiguration succeeded/
     end
 
