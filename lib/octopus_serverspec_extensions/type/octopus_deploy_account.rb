252865b5d588f503bcd0138f91b0090446253984
Jason Brown <stopthatastronaut@gmail.com>
2019-05-07 15:47:03 +1000

    Beginnings of Space support for Accounts

--- lib/octopus_serverspec_extensions/type/octopus_deploy_account.rb
+++ lib/octopus_serverspec_extensions/type/octopus_deploy_account.rb
@@ -8,12 +8,16 @@ module Serverspec::Type
     @target = nil
     @serverUrl = nil
     @apiKey = nil
+    @serverSupportsSpaces = nil
+    @spaceId = nil
+    @spaceFragment = ""
 
-    def initialize(serverUrl, apiKey, account_name)
+    def initialize(serverUrl, apiKey, account_name, spaceID = 'Spaces-1')
       @name = "Octopus Deploy Account #{account_name}"
       @runner = Specinfra::Runner
       @serverUrl = serverUrl
       @apiKey = apiKey
+      @spaceId = spaceID
 
       if (serverUrl.nil?)
         raise "'serverUrl' was not provided. Unable to connect to Octopus server to validate configuration."
@@ -23,6 +27,12 @@ module Serverspec::Type
       end
       if (account_name.nil?)
         raise "'account_name' was not provided. Unable to connect to Octopus server to validate configuration."
+      end
+
+      @serverSupportsSpaces = check_supports_spaces(serverUrl)
+
+      if (@serverSupportsSpaces)
+        @spaceFragment = "#{@spaceId}/"
       end
 
       @account = get_account_via_api(serverUrl, apiKey, account_name)
@@ -41,6 +51,16 @@ module Serverspec::Type
       return false if @account.nil?
       @account["AccountType"] == "AmazonWebServicesAccount"
     end
+
+    def in_space?(space_name)
+      return false if @account.nil?
+      return false if @serverSupportsSpaces
+      url = "#{@serverUrl}/api/#{@spaceFragment}spaces/all?api-key=#{@apiKey}"
+      resp = Net::HTTP.get_response(URI.parse(url))
+      spaces = JSON.parse(resp.body)
+      space_id = spaces.select {|e| e["Name"] == space_name}.first["Id"]
+      @machine["SpaceId"] == space_id
+    end
   end
 
   def octopus_deploy_account(serverUrl, apiKey, account_name)
@@ -51,14 +71,14 @@ module Serverspec::Type
 
   def get_account_via_api(serverUrl, apiKey, account_name)
     account = nil
-    url = "#{serverUrl}/api/accounts/all?api-key=#{apiKey}"
+    url = "#{serverUrl}/api/#{@spaceFragment}accounts/all?api-key=#{apiKey}"
 
     begin
       resp = Net::HTTP.get_response(URI.parse(url))
       body = JSON.parse(resp.body)
       account = body.select {|i| i['Name'] == account_name } unless body.nil?
     rescue => e
-      raise "Unable to connect to #{url}: #{e}"
+      raise "get_account_via_api: Unable to connect to #{url}: #{e}"
     end
 
     account
@@ -71,7 +91,7 @@ module Serverspec::Type
       version = body['Version']
       return Gem::Version.new(version) > Gem::Version.new('2019.0.0')
     rescue => e
-      puts "Unable to connect to #{serverUrl}: #{e}"
+      puts "check_supports_spaces: Unable to connect to #{serverUrl}: #{e}"
     end
 
     return false
--- spec/octopus/serverspec/type/octopus_deploy_account_spec.rb
+++ spec/octopus/serverspec/type/octopus_deploy_account_spec.rb
@@ -20,28 +20,78 @@ describe OctopusDeployAccount do
         to raise_error(/account_name/)
   end
 
-  example_account_found_response = File.open('spec/octopus/serverspec/json/accountsall.json')
+  context "Server does not support spaces" do
 
-  it "handles account found" do
-    stub_request(:get, "https://octopus.example.com/api/accounts/all?api-key=API-1234567890").
-        to_return(status: 200, body: example_account_found_response, headers: {})
-    wp = OctopusDeployAccount.new("https://octopus.example.com", "API-1234567890", "exampleorganisation-azure")
-    expect(wp.exists?).to be true
+    ex_does_not_support_spaces = File.read('spec/octopus/serverspec/json/apidoesnotsupportspaces.json')
+    ex_acc_found_response = File.read('spec/octopus/serverspec/json/accountsall.json')
+
+    it "handles account found" do
+      stub_request(:get, "https://octopus.example.com/api/").
+          to_return(status: 200, body: ex_does_not_support_spaces, headers: {})
+      stub_request(:get, "https://octopus.example.com/api/accounts/all?api-key=API-1234567890").
+          to_return(status: 200, body: ex_acc_found_response, headers: {})
+
+      wp = OctopusDeployAccount.new("https://octopus.example.com", "API-1234567890", "exampleorganisation-azure")
+      expect(wp.exists?).to be true
+    end
+
+    ex_acc_notfound_response = File.read('spec/octopus/serverspec/json/accountsall.json')
+
+    it "handles account not found" do
+      stub_request(:get, "https://octopus2.example.com/api/").
+          to_return(status: 200, body: ex_does_not_support_spaces, headers: {})
+      stub_request(:get, "https://octopus2.example.com/api/accounts/all?api-key=API-0987654321").
+          to_return(status: 200, body: ex_acc_notfound_response, headers: {})
+
+      wp = OctopusDeployAccount.new("https://octopus2.example.com", "API-0987654321", "Nonexistent Account")
+      expect(wp.exists?).to be false
+    end
+
+    it "doesn't crash badly if handed a bad URL" do
+      stub_request(:get, "https://nonexistentdomain.com/api/").
+          to_return(status: 200, body: ex_does_not_support_spaces, headers: {})
+      stub_request(:get, "https://nonexistentdomain.com/api/accounts/all?api-key=API-1234567890").to_raise(SocketError)
+
+      expect { OctopusDeployAccount.new("https://nonexistentdomain.com", "API-1234567890", "exampleorganisation-azure") }.to raise_error(StandardError)
+    end
+
   end
 
-  example_account_notfound_response = File.open('spec/octopus/serverspec/json/accountsall.json')
+  context "Server supports spaces" do
 
-  it "handles account not found" do
-    stub_request(:get, "https://octopus2.example.com/api/accounts/all?api-key=API-0987654321").
-        to_return(status: 200, body: example_account_notfound_response, headers: {})
-    wp = OctopusDeployAccount.new("https://octopus2.example.com", "API-0987654321", "Nonexistent Account")
-    expect(wp.exists?).to be false
-  end
+    ex_supports_spaces = File.read('spec/octopus/serverspec/json/apisupportsspaces.json')
+    ex_account_found_response = File.read('spec/octopus/serverspec/json/accountsall.json')
 
-  it "doesn't crash badly if handed a bad URL" do
-    stub_request(:get, "https://nonexistentdomain.com/api/accounts/all?api-key=API-1234567890").to_raise(SocketError)
+    it "handles account found" do
+      stub_request(:get, "https://octopus.example.com/api/").
+          to_return(status: 200, body: ex_supports_spaces, headers: {})
+      stub_request(:get, "https://octopus.example.com/api/Spaces-1/accounts/all?api-key=API-1234567890").
+          to_return(status: 200, body: ex_account_found_response, headers: {})
 
-    expect { OctopusDeployAccount.new("https://nonexistentdomain.com", "API-1234567890", "exampleorganisation-azure") }.to raise_error(StandardError)
+      wp = OctopusDeployAccount.new("https://octopus.example.com", "API-1234567890", "exampleorganisation-azure")
+      expect(wp.exists?).to be true
+    end
+
+    ex_account_notfound_response = File.read('spec/octopus/serverspec/json/accountsall.json')
+
+    it "handles account not found" do
+      stub_request(:get, "https://octopus2.example.com/api/").
+          to_return(status: 200, body: ex_supports_spaces, headers: {})
+      stub_request(:get, "https://octopus2.example.com/api/Spaces-1/accounts/all?api-key=API-0987654321").
+          to_return(status: 200, body: ex_account_notfound_response, headers: {})
+
+      wp = OctopusDeployAccount.new("https://octopus2.example.com", "API-0987654321", "Nonexistent Account")
+      expect(wp.exists?).to be false
+    end
+
+    it "doesn't crash badly if handed a bad URL" do
+      stub_request(:get, "https://nonexistentdomain.com/api/").
+          to_return(status: 200, body: ex_supports_spaces, headers: {})
+      stub_request(:get, "https://nonexistentdomain.com/api/Spaces-1/accounts/all?api-key=API-1234567890").to_raise(SocketError)
+
+      expect { OctopusDeployAccount.new("https://nonexistentdomain.com", "API-1234567890", "exampleorganisation-azure") }.to raise_error(StandardError)
+    end
+
   end
 
 end
