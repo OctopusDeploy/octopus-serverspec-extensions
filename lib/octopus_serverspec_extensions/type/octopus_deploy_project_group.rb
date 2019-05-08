cbd9b119237fb248b116f61e1937592261197636
Jason Brown <stopthatastronaut@gmail.com>
2019-05-08 16:04:26 +1000

    Project Group tests - beginnings

---
+++ lib/octopus_serverspec_extensions/type/octopus_deploy_project_group.rb
@@ -0,0 +1,105 @@
+require 'serverspec/type/base'
+require 'net/http'
+require 'json'
+
+module Serverspec::Type
+  class OctopusDeployProjectGroup < Base
+    @projectgroup = nil
+    @serverUrl = nil
+    @apiKey = nil
+    @serverSupportsSpaces = nil
+    @spaceId = nil
+    @spaceFragment = ""
+
+    def initialize(serverUrl, apiKey, projectgroup_name, spaceID = 'Spaces-1')
+      @name = "Octopus Deploy Project Group #{projectgroup_name}"
+      @runner = Specinfra::Runner
+      @serverUrl = serverUrl
+      @apiKey = apiKey
+      @spaceId = spaceID
+
+      if serverUrl.nil?
+        raise "'serverUrl' was not provided. Unable to connect to Octopus server to validate configuration."
+      end
+      if apiKey.nil?
+        raise "'apiKey' was not provided. Unable to connect to Octopus server to validate configuration."
+      end
+      if projectgroup_name.nil?
+        raise "'projectgroup_name' was not provided. Unable to connect to Octopus server to validate configuration."
+      end
+
+      @serverSupportsSpaces = check_supports_spaces(serverUrl)
+
+      if @serverSupportsSpaces
+        @spaceFragment = "#{@spaceId}/"
+      end
+
+      @projectgroup = get_projectgroup_via_api(serverUrl, apiKey, projectgroup_name)
+    end
+
+    def exists?
+      (!@projectgroup.nil?) && (@projectgroup != [])
+    end
+
+    def has_description?(projectgroup_description)
+      return false if @projectgroup.nil?
+      @projectgroup["Description"] == projectgroup_description
+    end
+
+    def in_space?(space_name)
+      return false if @projectgroup.nil?
+      return false if @serverSupportsSpaces
+      url = "#{@serverUrl}/api/#{@spaceFragment}spaces/all?api-key=#{@apiKey}"
+      resp = Net::HTTP.get_response(URI.parse(url))
+      spaces = JSON.parse(resp.body)
+      space_id = spaces.select {|e| e["Name"] == space_name}.first["Id"]
+      @machine["SpaceId"] == space_id
+    end
+
+    def in_environment?(environment_name)
+      return false if @projectgroup.nil?
+      url = "#{@serverUrl}/api/#{@spaceFragment}environments/all?api-key=#{@apiKey}"
+      resp = Net::HTTP.get_response(URI.parse(url))
+      environments = JSON.parse(resp.body)
+      environment_id = environments.select {|e| e["Name"] == environment_name}.first["Id"]
+      !@projectgroup["EnvironmentIds"].select {|e| e == environment_id}.empty?
+    end
+  end
+
+  def octopus_deploy_projectgroup(serverUrl, apiKey, projectgroup_name)
+    OctopusDeployProjectGroup.new(serverUrl, apiKey, projectgroup_name)
+  end
+
+  private
+
+  def get_projectgroup_via_api(serverUrl, apiKey, projectgroup_name)
+    projectgroup = nil
+    url = "#{serverUrl}/api/#{@spaceFragment}projectgroups/all?api-key=#{apiKey}"
+
+    begin
+      resp = Net::HTTP.get_response(URI.parse(url))
+      body = JSON.parse(resp.body)
+      projectgroup = body.select {|i| i['Name'] == projectgroup_name } unless body.nil?
+    rescue => e
+      raise "get_projectgroup_via_api: Unable to connect to #{url}: #{e}"
+    end
+
+    projectgroup[0] # it's an array, and we need the first object from it
+  end
+
+  def check_supports_spaces(serverUrl)
+    begin
+      resp = Net::HTTP.get_response(URI.parse("#{serverUrl}/api/"))
+      body = JSON.parse(resp.body)
+      version = body['Version']
+      return Gem::Version.new(version) > Gem::Version.new('2019.0.0')
+    rescue => e
+      puts "check_supports_spaces: Unable to connect to #{serverUrl}: #{e}"
+    end
+
+    false
+  end
+
+end
+
+include Serverspec::Type
---
+++ spec/octopus/serverspec/type/octopus_deploy_project_group_spec.rb
@@ -0,0 +1,112 @@
+require 'spec_helper'
+require 'webmock/rspec'
+
+describe OctopusDeployProjectGroup do
+
+  let(:runner) { double ("runner")}
+
+  it "throws if `serverUrl` not supplied" do
+    expect { OctopusDeployProjectGroup.new(nil, "someapikey", "my new projectgroup") }.
+        to raise_error(/serverUrl/)
+  end
+
+  it "throws if `apiKey` not supplied" do
+    expect { OctopusDeployProjectGroup.new("https://someserver.com", nil, "my new projectgroup") }.
+        to raise_error(/apiKey/)
+  end
+
+  it "throws if `ProjectGroupName` not supplied" do
+    expect { OctopusDeployProjectGroup.new("https://someserver.com", "API-kllkjhasdkljhasdfkjsafd", nil) }.
+        to raise_error(/projectgroup_name/)
+  end
+
+  ex_does_not_support_spaces = File.read('spec/octopus/serverspec/json/apidoesnotsupportspaces.json')
+  ex_acc_found_response = File.read('spec/octopus/serverspec/json/projectgroupsall.json')
+
+  context "Server does not support spaces" do
+
+    ex_does_not_support_spaces = File.read('spec/octopus/serverspec/json/apidoesnotsupportspaces.json')
+    ex_acc_found_response = File.read('spec/octopus/serverspec/json/projectgroupsall.json')
+
+    it "handles projectgroup found" do
+      stub_request(:get, "https://octopus.example.com/api/").
+          to_return(status: 200, body: ex_does_not_support_spaces, headers: {})
+      stub_request(:get, "https://octopus.example.com/api/projectgroups/all?api-key=API-1234567890").
+          to_return(status: 200, body: ex_acc_found_response, headers: {})
+
+      wp = OctopusDeployProjectGroup.new("https://octopus.example.com", "API-1234567890", "Octopus Projects")
+      expect(wp.exists?).to be true
+    end
+
+    ex_acc_notfound_response = File.read('spec/octopus/serverspec/json/projectgroupsall.json')
+
+    it "handles projectgroup not found" do
+      stub_request(:get, "https://octopus2.example.com/api/").
+          to_return(status: 200, body: ex_does_not_support_spaces, headers: {})
+      stub_request(:get, "https://octopus2.example.com/api/projectgroups/all?api-key=API-0987654321").
+          to_return(status: 200, body: ex_acc_notfound_response, headers: {})
+
+      wp = OctopusDeployProjectGroup.new("https://octopus2.example.com", "API-0987654321", "Nonexistent projectgroup")
+      expect(wp.exists?).to be false
+    end
+
+    it "doesn't crash badly if handed a bad URL" do
+      stub_request(:get, "https://nonexistentdomain.com/api/").
+          to_return(status: 200, body: ex_does_not_support_spaces, headers: {})
+      stub_request(:get, "https://nonexistentdomain.com/api/projectgroups/all?api-key=API-1234567890").to_raise(SocketError)
+
+      expect { OctopusDeployProjectGroup.new("https://nonexistentdomain.com", "API-1234567890", "Octopus Projects") }.to raise_error(StandardError)
+    end
+
+  end
+
+  context "Server supports spaces" do
+
+    ex_supports_spaces = File.read('spec/octopus/serverspec/json/apisupportsspaces.json')
+    ex_projectgroup_found_response = File.read('spec/octopus/serverspec/json/projectgroupsall.json')
+
+    it "handles projectgroup found" do
+      stub_request(:get, "https://octopus.example.com/api/").
+          to_return(status: 200, body: ex_supports_spaces, headers: {})
+      stub_request(:get, "https://octopus.example.com/api/Spaces-1/projectgroups/all?api-key=API-1234567890").
+          to_return(status: 200, body: ex_projectgroup_found_response, headers: {})
+
+      wp = OctopusDeployProjectGroup.new("https://octopus.example.com", "API-1234567890", "Octopus Projects")
+
+      expect(wp.exists?).to be true
+    end
+
+    ex_projectgroup_notfound_response = File.read('spec/octopus/serverspec/json/projectgroupsall.json')
+
+    it "handles projectgroup not found" do
+      stub_request(:get, "https://octopus2.example.com/api/").
+          to_return(status: 200, body: ex_supports_spaces, headers: {})
+      stub_request(:get, "https://octopus2.example.com/api/Spaces-1/projectgroups/all?api-key=API-0987654321").
+          to_return(status: 200, body: ex_projectgroup_notfound_response, headers: {})
+
+      wp = OctopusDeployProjectGroup.new("https://octopus2.example.com", "API-0987654321", "Nonexistent projectgroup")
+      expect(wp.exists?).to be false
+    end
+
+    it "doesn't crash badly if handed a bad URL" do
+      stub_request(:get, "https://nonexistentdomain.com/api/").
+          to_return(status: 200, body: ex_supports_spaces, headers: {})
+      stub_request(:get, "https://nonexistentdomain.com/api/Spaces-1/projectgroups/all?api-key=API-1234567890").to_raise(SocketError)
+
+      expect { OctopusDeployProjectGroup.new("https://nonexistentdomain.com", "API-1234567890", "Octopus Projects") }.to raise_error(StandardError)
+    end
+
+    it "Correctly checks the 'description' field" do
+      stub_request(:get, "https://octopus2.example.com/api/").
+          to_return(status: 200, body: ex_supports_spaces, headers: {})
+      stub_request(:get, "https://octopus2.example.com/api/Spaces-1/projectgroups/all?api-key=API-0987654321").
+          to_return(status: 200, body: ex_projectgroup_notfound_response, headers: {})
+
+
+      wp = OctopusDeployProjectGroup.new("https://octopus2.example.com", "API-0987654321", "Octopus Projects", "Spaces-1")
+      expect(wp.has_description?("This is an example Azure Subscription")).to be true
+    end
+
+  end
+
+end 
