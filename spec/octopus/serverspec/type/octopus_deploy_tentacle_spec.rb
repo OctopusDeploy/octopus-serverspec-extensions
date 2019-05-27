11dc608e573caade95cf0be1d7e0904979cbe61b
Jason Brown <stopthatastronaut@gmail.com>
2019-05-27 15:10:49 +1000

    Test polling_tentacle? and listening_tentacle? under spaces

--- spec/octopus/serverspec/type/octopus_deploy_tentacle_spec.rb
+++ spec/octopus/serverspec/type/octopus_deploy_tentacle_spec.rb
@@ -26,13 +26,15 @@ describe OctopusDeployTentacle do
   context "Server supports spaces" do
 
     ex_supports_spaces = get_api_example('/api/2019.4.5')
+    before(:each) do
+      allow(File).to receive(:exists?).and_return(true)
+      stub_request(:get, "https://octopus.example.com/api/").
+          to_return(status: 200, body: ex_supports_spaces, headers: {})
+    end
 
     # .registered_with_the_server()
     it "returns false if the tentacle is not registered to the server" do
-      allow(File).to receive(:exists?).and_return(true)
       allow_any_instance_of(OctopusDeployTentacle).to receive(:`).and_return("D7E6B4CEEE0960CE944B92432605A2BAF14C7405")
-      stub_request(:get, "https://octopus.example.com/api/").
-          to_return(status: 200, body: ex_supports_spaces, headers: {})
       stub_request(:get, "https://octopus.example.com/api/Spaces-1/machines/all?api-key=API-1234567890").
           to_return(status: 200, body: example_tentacle_response, headers: {})
 
@@ -41,10 +43,7 @@ describe OctopusDeployTentacle do
     end
 
     it "returns true if the tentacle is registered to the server" do
-      allow(File).to receive(:exists?).and_return(true)
       allow_any_instance_of(OctopusDeployTentacle).to receive(:`).and_return("4BD377BF21A882251B9A5A492889555377A58E07")
-      stub_request(:get, "https://octopus.example.com/api/").
-          to_return(status: 200, body: ex_supports_spaces, headers: {})
       stub_request(:get, "https://octopus.example.com/api/Spaces-1/machines/all?api-key=API-1234567890").
           to_return(status: 200, body: example_tentacle_response, headers: {})
 
@@ -54,10 +53,7 @@ describe OctopusDeployTentacle do
 
     # .online?
     it "returns true for a machine that's known to be online" do
-      allow(File).to receive(:exists?).and_return(true)
       allow_any_instance_of(OctopusDeployTentacle).to receive(:`).and_return("4BD377BF21A882251B9A5A492889555377A58E07")
-      stub_request(:get, "https://octopus.example.com/api/").
-          to_return(status: 200, body: ex_supports_spaces, headers: {})
       stub_request(:get, "https://octopus.example.com/api/Spaces-1/machines/all?api-key=API-1234567890").
           to_return(status: 200, body: example_tentacle_response, headers: {})
 
@@ -65,18 +61,56 @@ describe OctopusDeployTentacle do
       expect(nt.online?).to be true
     end
 
+    # .listening_tentacle?
+    it "returns true for a known listening tentacle" do
+      allow_any_instance_of(OctopusDeployTentacle).to receive(:`).and_return("D577F1B4D70D24E1356EF5B75CD7542BB049A073")
+      stub_request(:get, "https://octopus.example.com/api/Spaces-1/machines/all?api-key=API-1234567890").
+          to_return(status: 200, body: example_tentacle_response, headers: {})
+      lt = OctopusDeployTentacle.new('https://octopus.example.com', 'API-1234567890', 'ListeningTentacle')
+      expect(lt.listening_tentacle?).to be true
+    end
+
+    it "returns false for a known polling tentacle" do
+      allow_any_instance_of(OctopusDeployTentacle).to receive(:`).and_return("2926388491F714807F0B181B38DBB9AA1EF946DC")
+      stub_request(:get, "https://octopus.example.com/api/Spaces-1/machines/all?api-key=API-1234567890").
+          to_return(status: 200, body: example_tentacle_response, headers: {})
+
+      pt = OctopusDeployTentacle.new('https://octopus.example.com', 'API-1234567890', 'PollingTentacle')
+      expect(pt.listening_tentacle?).to be false
+    end
+
+    # .polling_tentacle?
+    it "returns true for a known polling tentacle" do
+      allow_any_instance_of(OctopusDeployTentacle).to receive(:`).and_return("2926388491F714807F0B181B38DBB9AA1EF946DC")
+      stub_request(:get, "https://octopus.example.com/api/Spaces-1/machines/all?api-key=API-1234567890").
+          to_return(status: 200, body: example_tentacle_response, headers: {})
+
+      pt = OctopusDeployTentacle.new('https://octopus.example.com', 'API-1234567890', 'PollingTentacle')
+      expect(pt.polling_tentacle?).to be true
+    end
+
+    it "returns false for a known listening tentacle" do
+      allow_any_instance_of(OctopusDeployTentacle).to receive(:`).and_return("D577F1B4D70D24E1356EF5B75CD7542BB049A073")
+      stub_request(:get, "https://octopus.example.com/api/Spaces-1/machines/all?api-key=API-1234567890").
+          to_return(status: 200, body: example_tentacle_response, headers: {})
+      lt = OctopusDeployTentacle.new('https://octopus.example.com', 'API-1234567890', 'ListeningTentacle')
+      expect(lt.polling_tentacle?).to be false
+    end
 
   end
 
   context "server does not support spaces" do
     ex_does_not_support_spaces = get_api_example('/api/2018.7.9')
 
-    # .registered_with_the_server()
-    it "returns false if the tentacle is not registered to the server" do
+    before(:each) do
       allow(File).to receive(:exists?).and_return(true)
-      allow_any_instance_of(OctopusDeployTentacle).to receive(:`).and_return("D7E6B4CEEE0960CE944B92432605A2BAF14C7405")
       stub_request(:get, "https://octopus.example.com/api/").
           to_return(status: 200, body: ex_does_not_support_spaces, headers: {})
+    end
+
+    # .registered_with_the_server()
+    it "returns false if the tentacle is not registered to the server" do
+      allow_any_instance_of(OctopusDeployTentacle).to receive(:`).and_return("D7E6B4CEEE0960CE944B92432605A2BAF14C7405")
       stub_request(:get, "https://octopus.example.com/api/machines/all?api-key=API-1234567890").
           to_return(status: 200, body: example_tentacle_response, headers: {})
 
@@ -85,15 +119,22 @@ describe OctopusDeployTentacle do
     end
 
     it "returns true if the tentacle is registered to the server" do
-      allow(File).to receive(:exists?).and_return(true)
       allow_any_instance_of(OctopusDeployTentacle).to receive(:`).and_return("4BD377BF21A882251B9A5A492889555377A58E07")
-      stub_request(:get, "https://octopus.example.com/api/").
-          to_return(status: 200, body: ex_does_not_support_spaces, headers: {})
       stub_request(:get, "https://octopus.example.com/api/machines/all?api-key=API-1234567890").
           to_return(status: 200, body: example_tentacle_response, headers: {})
 
       nt = OctopusDeployTentacle.new('https://octopus.example.com', 'API-1234567890', 'ListeningTentacleWithThumbprintWithoutAutoRegister')
       expect(nt.registered_with_the_server?).to be true
+    end
+
+    # .online?
+    it "returns true for a machine that's known to be online" do
+      allow_any_instance_of(OctopusDeployTentacle).to receive(:`).and_return("4BD377BF21A882251B9A5A492889555377A58E07")
+      stub_request(:get, "https://octopus.example.com/api/machines/all?api-key=API-1234567890").
+          to_return(status: 200, body: example_tentacle_response, headers: {})
+
+      nt = OctopusDeployTentacle.new('https://octopus.example.com', 'API-1234567890', 'ListeningTentacleWithThumbprintWithoutAutoRegister')
+      expect(nt.online?).to be true
     end
 
   end
