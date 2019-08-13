b46535348d9f5597a1044b668a74b59574fc1c07
Jason Brown <stopthatastronaut@users.noreply.github.com>
2019-08-13 15:10:15 +1000

    Split PR Part 4: Enhance Project groups - spaces support/new auth (#15)
    * Enhance Project groups - spaces support/new auth
    
    * Update docs, remove two files from 'Environment' resource (to follow)
    
    * Couple of small linter changes
    
    * PR feedback
    
    * PRfeedback and a tiny refactor
    
    * tiny fomratting tweak

--- lib/octopus_serverspec_extensions/type/octopus_deploy_project_group.rb
+++ lib/octopus_serverspec_extensions/type/octopus_deploy_project_group.rb
@@ -4,100 +4,112 @@ require 'json'
 
 module Serverspec::Type
   class OctopusDeployProjectGroup < Base
-    @projectgroup = nil
+    @project_group = nil
+    @project_group_name = nil
     @serverUrl = nil
     @apiKey = nil
     @serverSupportsSpaces = nil
     @spaceId = nil
     @spaceFragment = ""
 
-    def initialize(serverUrl, apiKey, projectgroup_name, space_name = nil)
-      @name = "Octopus Deploy Project Group #{projectgroup_name}"
+    def initialize(*url_and_api_key, project_group_name)
+      serverUrl, apiKey = get_octopus_creds(url_and_api_key)
+
+      raise "'project_group_name' was not provided. Unable to connect to Octopus server to validate configuration."  if @project_group_name.nil?
+
+      @project_group_name = project_group_name
+
+      @name = "Octopus Deploy Project Group #{project_group_name}"
       @runner = Specinfra::Runner
       @serverUrl = serverUrl
       @apiKey = apiKey
 
-
-      if serverUrl.nil?
-        raise "'serverUrl' was not provided. Unable to connect to Octopus server to validate configuration."
-      end
-      if apiKey.nil?
-        raise "'apiKey' was not provided. Unable to connect to Octopus server to validate configuration."
-      end
-      if projectgroup_name.nil?
-        raise "'projectgroup_name' was not provided. Unable to connect to Octopus server to validate configuration."
-      end
-
       @serverSupportsSpaces = check_supports_spaces(serverUrl)
-
-      if @serverSupportsSpaces
-        # set the spaceId correctly
-
-        if space_name.nil?
-          @spaceId = 'Spaces-1' # default to Spaces-1
-        else
-          @spaceId = get_space_id?(space_name)
-        end
-
-        @spaceFragment = "#{@spaceId}/"
-      end
-
-      @projectgroup = get_projectgroup_via_api(serverUrl, apiKey, projectgroup_name)
     end
 
     def exists?
-      (!@projectgroup.nil?) && (@projectgroup != [])
+      load_resource_if_nil
+      (!@project_group.nil?) && (@project_group != [])
     end
 
-    def has_description?(projectgroup_description)
-      return false if @projectgroup.nil?
-      @projectgroup["Description"] == projectgroup_description
+    def has_description?(project_group_description)
+      load_resource_if_nil
+      return false if @project_group.nil?
+      @project_group["Description"] == project_group_description
+    end
+
+    def in_space(space_name)
+      # allows us to tag .in_space() onto the end of the resource. as in
+      # describe octopus_project_group("group name").in_space("MyNewSpace") do
+      @spaceId = get_space_id(space_name)
+
+      raise "Unable to resolve Space Id for space name '#{space_name}'." if @spaceId.nil?
+      self
+    end
+
+    private
+
+    def get_space_id(space_name)
+      return false if @serverSupportsSpaces.nil?
+      url = "#{@serverUrl}/api/Spaces/all?api-key=#{@apiKey}"
+      resp = Net::HTTP.get_response(URI.parse(url))
+      spaces = JSON.parse(resp.body)
+      space_id = spaces.select {|e| e["Name"] == space_name}.first["Id"]
+      space_id
+    end
+
+    def load_resource_if_nil
+      if @project_group.nil?
+        @project_group = get_project_group_via_api(@serverUrl, @apiKey, @project_group_name)
+      end
     end
   end
 
-  def octopus_deploy_projectgroup(serverUrl, apiKey, projectgroup_name, spaceName = nil)
-    OctopusDeployProjectGroup.new(serverUrl, apiKey, projectgroup_name, spaceName)
+  # module-level constructors/entrypoints
+
+  def octopus_deploy_projectgroup(*url_and_api_key, project_group_name) # deprecated - no underscore in name
+    serverUrl, apiKey = get_octopus_creds(url_and_api_key)
+    OctopusDeployProjectGroup.new(serverUrl, apiKey, project_group_name)
+  end
+
+  def octopus_deploy_project_group(*url_and_api_key, project_group_name)
+    url, apikey = get_octopus_creds(url_and_api_key)
+    octopus_deploy_projectgroup(url, apikey, project_group_name)
+  end
+
+  def octopus_project_group(*url_and_api_key, project_group_name)
+    url, apikey = get_octopus_creds(url_and_api_key)
+    octopus_deploy_projectgroup(url, apikey, project_group_name)
+  end
+
+  def octopus_projectgroup(*url_and_api_key, project_group_name) # deprecated - no underscore in name
+    url, apikey = get_octopus_creds(url_and_api_key)
+    octopus_deploy_projectgroup(url, apikey, project_group_name)
   end
 
   private
 
-  def get_projectgroup_via_api(serverUrl, apiKey, projectgroup_name)
+  def get_project_group_via_api(serverUrl, apiKey, project_group_name)
     pg = nil
+
+   raise "'project_group_name' not supplied" if (project_group_name.nil? || project_group_name == '')
+
+    unless @spaceId.nil?
+      @spaceFragment = "#{@spaceId}/"
+    end
+
     url = "#{serverUrl}/api/#{@spaceFragment}projectgroups/all?api-key=#{apiKey}"
 
     begin
       resp = Net::HTTP.get_response(URI.parse(url))
       body = JSON.parse(resp.body)
-      pg = body.select {|i| i['Name'] == projectgroup_name }.first unless body.nil?
+      pg = body.select {|i| i['Name'] == project_group_name }.first unless body.nil?
     rescue => e
-      raise "get_projectgroup_via_api: Unable to connect to #{url}: #{e}"
+      raise "get_project_group_via_api: Unable to connect to #{url}: #{e}"
     end
 
     pg
   end
-
-  def check_supports_spaces(serverUrl)
-    begin
-      resp = Net::HTTP.get_response(URI.parse("#{serverUrl}/api/"))
-      body = JSON.parse(resp.body)
-      version = body['Version']
-      return Gem::Version.new(version) > Gem::Version.new('2019.0.0')
-    rescue => e
-      puts "check_supports_spaces: Unable to connect to #{serverUrl}: #{e}"
-    end
-
-    false
-  end
-
-  def get_space_id?(space_name)
-    return false if @serverSupportsSpaces.nil?
-    url = "#{@serverUrl}/api/Spaces/all?api-key=#{@apiKey}"
-    resp = Net::HTTP.get_response(URI.parse(url))
-    spaces = JSON.parse(resp.body)
-    space_id = spaces.select {|e| e["Name"] == space_name}.first["Id"]
-    space_id
-  end
-
 end
 
 include Serverspec::Type
