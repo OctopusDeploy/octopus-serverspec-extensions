1438b1a651a25de3f1fd417ae6901c57f4e570dc
Jason Brown <stopthatastronaut@users.noreply.github.com>
2019-08-15 09:44:37 +1000

    Not the Split PR but a new one: An upgrade config type (#18)
    * An upgrade config type
    
    * Start on docs
    
    * Add docs
    
    * tweak docs
    
    * Change url to use a varaible drawn from the hypermedia links

---
+++ spec/octopus/serverspec/type/octopus_deploy_upgrade_config_spec.rb
@@ -0,0 +1,54 @@
+require 'spec_helper'
+require 'webmock/rspec'
+
+describe OctopusDeployUpgradeConfig do
+
+  let(:runner) { double ("runner")}
+
+  example_config_response = get_api_example('/api/upgradeconfiguration')
+  example_config_response2 = get_api_example('/api/upgradeconfiguration2')
+  example_root_response = get_api_example('/api/2019.4.5')
+
+  before(:each) do
+    stub_request(:get, "https://octopus.example.com/api/upgradeconfiguration?api-key=API-1234567890").
+        to_return(status: 200, body: example_config_response, headers: {})
+    stub_request(:get, "https://octopus.example.com/api/").
+        to_return(status: 200, body: example_root_response, headers: {})
+  end
+
+  it 'should handle being give no creds' do
+    allow_any_instance_of(OctopusDeployUpgradeConfig).to receive(:get_env_var).with('OCTOPUS_CLI_API_KEY').and_return("API-1234567890")
+    allow_any_instance_of(OctopusDeployUpgradeConfig).to receive(:get_env_var).with('OCTOPUS_CLI_SERVER').and_return("https://octopus.example.com")
+
+    my_upgrade_config = OctopusDeployUpgradeConfig.new()
+    expect( my_upgrade_config.always_show_notifications? ).to be true
+  end
+
+  it 'should be able to detect NotificationMode' do
+    my_upgrade_config = OctopusDeployUpgradeConfig.new("https://octopus.example.com", "API-1234567890")
+    expect( my_upgrade_config.always_show_notifications? ).to be true
+    expect( my_upgrade_config.never_show_notifications? ).to be false
+    expect( my_upgrade_config.show_major_minor_notifications?).to be false
+  end
+
+  it 'should be able to detect NotificationMode (inverse test)' do
+    stub_request(:get, "https://octopus.example.com/api/upgradeconfiguration?api-key=API-1234567890").
+        to_return(status: 200, body: example_config_response2, headers: {})
+
+    my_upgrade_config = OctopusDeployUpgradeConfig.new("https://octopus.example.com", "API-1234567890")
+    expect( my_upgrade_config.always_show_notifications? ).to be false
+    expect( my_upgrade_config.never_show_notifications? ).to be true
+    expect( my_upgrade_config.show_major_minor_notifications?).to be false
+  end
+
+  it 'should be able to detect AllowChecking' do
+    my_upgrade_config = OctopusDeployUpgradeConfig.new("https://octopus.example.com", "API-1234567890")
+    expect( my_upgrade_config.allow_checking?).to be true
+  end
+
+  it 'should be able to detect IncludeStatistics' do
+    my_upgrade_config = OctopusDeployUpgradeConfig.new("https://octopus.example.com", "API-1234567890")
+    expect( my_upgrade_config.include_statistics?).to be true
+  end
+end
+
