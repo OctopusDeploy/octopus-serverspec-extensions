ed6af81c5998ed536a9ea016fe53b217bdb0b390
Matt Richardson <matt.richardson@octopus.com>
2020-04-14 07:38:10 +1000

    Tweak dsc type names

--- lib/octopus_serverspec_extensions/type/windows_dsc.rb
+++ lib/octopus_serverspec_extensions/type/windows_dsc.rb
@@ -13,12 +13,12 @@ module Serverspec::Type
       command_result.stdout.gsub(/\n/, '').match /Get-DSCConfiguration succeeded/
     end
 
-    def return_true_for_test_dsc_configuration?
+    def has_test_dsc_configuration_return_true?
       command_result = @runner.run_command('$ProgressPreference = "SilentlyContinue"; $state = ""; do { $state = (Get-DscLocalConfigurationManager).LCMState; write-host "LCM state is $state"; Start-Sleep -Seconds 2; } while ($state -ne "Idle"); try { if (-not (Test-DSCConfiguration -ErrorAction Stop)) { write-output "Test-DSCConfiguration returned false"; exit 1 } write-output "Test-DSCConfiguration succeeded"; exit 0 } catch { write-output "Test-DSCConfiguration failed"; write-output $_; exit 2 }')
       command_result.stdout.gsub(/\n/, '').match /Test-DSCConfiguration succeeded/
     end
 
-    def has_test_dsc_configuration_return_success?
+    def has_dsc_configuration_status_of_success?
       command_result = @runner.run_command('$ProgressPreference = "SilentlyContinue"; try { $statuses = @(Get-DSCConfigurationStatus -ErrorAction Stop -All); $status = $statuses[0].Status; write-host "Get-DSCConfigurationStatus is \'$status\'"; if ($status -eq "Success") { exit 0 } else { exit 1 } } catch { write-output "Get-DSCConfigurationStatus failed"; write-output $_; exit 2; }')
       command_result.stdout.gsub(/\n/, '').match /Get-DSCConfigurationStatus is 'Success'/
     end
